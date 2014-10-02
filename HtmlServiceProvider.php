538c9e5051c47bdd77b93af23f00238959c463cf
Alexander Obuhovich <aik.bold@gmail.com>
2014-10-02 12:17:46 +0300

    Expose all classes, that `HtmlServiceProvider` is responsible for
    Because this service provider is differed we need to expose fact, that it's responsible for handling `HtmlBuilder` and `FormBuilder` classes. Without this the corresponding `form` and `html` aliases won't be registered before they are used during Container dependency resolution.

--- HtmlServiceProvider.php
+++ HtmlServiceProvider.php
@@ -61,7 +61,7 @@ class HtmlServiceProvider extends ServiceProvider {
 	 */
 	public function provides()
 	{
-		return array('html', 'form');
+		return array('html', 'form', 'Illuminate\Html\HtmlBuilder', 'Illuminate\Html\FormBuilder');
 	}
 
 }
